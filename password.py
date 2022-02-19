221cf79ca3b3f1ede61db19b9956b39378eee54f
John-Kimani <kimwaweru100@gmail.com>
2022-02-19 21:08:49 +0300

    creates User and Credential classes

---
+++ password.py
@@ -0,0 +1,12 @@
+class User:
+    '''
+    This class helps generate instances for creating a new account on this appliation
+    '''
+
+
+
+## generate credentials instances
+class Credential:
+    '''
+    This class helps generate instances for new and or existing online accounts log in credentials
+    '''
