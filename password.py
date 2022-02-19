b766681eaeece97c1d0f1f262cf0214e4a944450
John-Kimani <kimwaweru100@gmail.com>
2022-02-19 21:21:50 +0300

    creates class (list) and init instance variables

--- password.py
+++ password.py
@@ -10,3 +10,18 @@ class Credential:
     '''
     This class helps generate instances for new and or existing online accounts log in credentials
     '''
+
+    credentials_list = [] # empty list that will hold online accounts log in details
+
+    def __init__(self, accountName, accountUsername, accountPassword):
+        '''
+        This function helps define properties of new and or existing accounts objects.
+        Args:
+            accountName: brand name of user online account. For example; 'pintrest'
+            accountUsername: user username of a specified online account.
+            accountPassword: user password for an online account.
+        '''
+
+        self.accountName = accountName
+        self.accountUsername = accountUsername
+        self.accountPassword = accountPassword
