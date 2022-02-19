2fb85ca336c6af79934bbb815b3ce4437cbb1f22
John-Kimani <kimwaweru100@gmail.com>
2022-02-20 02:53:25 +0300

    creates navigation codes and funtionalities to display, search contacts and exit app

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.6
 
+from dis import show_code
 from password import Credential
 
 def create_account(accountName, accountUsername, accountPassword):
@@ -40,5 +41,66 @@ def main():
     print('\n')
 
     while True:
-        print("Use these short codes: cc - create a new account, da - display your saved accounts, fa - find an account, ex - exit the account log in credentials list")
-        short_code == input().lower()
+        print("Use these short codes: ca - create a new account, da - display your saved accounts, fa - find an account, ex - exit the account log in account credential list")
+        
+        short_code = input().lower() #sets a variable to store short codes navigating this app
+
+        #create account condition
+        if short_code == 'ca':
+            print('New account')
+            print('-'*10)
+
+            print("Account Brand name e.g: 'Pintrest' ... ")
+            accountName = input()
+
+            print('Your account username ...')
+            accountUsername = input()
+
+            print('Keyin Password ...')
+            accountPassword = input()
+
+            save_account(create_account(accountName, accountUsername, accountPassword))# creates and save new account
+
+            print('\n')
+            print(f"New {accountName} account  with username {accountUsername} has been created successfully")
+            print('\n')
+
+            #display account
+        elif short_code == 'da':
+
+            if display_accounts():
+                print("Here is alist of all your saved account log in credentials")
+                print('\n')
+
+                for account in display_accounts():
+                    print(f"{account.accountName} {account.accountUsername}")
+                    print('\n')
+            else:
+                print('\n')
+                print("You dont seem to have any accounts saved yet")
+                print('\n')
+                # find account
+        elif short_code == 'fa':
+            print('Enter the username you want to search for')
+            search_accountUsername = input()
+            if check_existing_account(search_accountUsername):
+                search_account = find_account(search_accountUsername)
+                print(f"{search_account.accountName} {search_account.accountUsername} {search_account.accountPassword}")
+                print('-'*20)
+
+                print(f"Account name: {search_account.accountName}")
+                print(f"Account username: {search_account.accountUsername}")
+            else:
+                print("That account does not exist")
+    #exit app
+        elif short_code == 'ex':
+            print('Thank you for considering our service. Goodbye for now see you later!')
+            break
+        else:
+            print('I really didnt get that. Please use the short codes')
+
+
+
+if __name__ == '__main__':
+    main()
+
