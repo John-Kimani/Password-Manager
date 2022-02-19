200b58eb45cf538da50467f1c7166f714e775d32
John-Kimani <kimwaweru100@gmail.com>
2022-02-19 21:39:03 +0300

    Imports unitest for test cases, Identifies user BDD

---
+++ password_test.py
@@ -0,0 +1,10 @@
+import unittest #unitest module
+from password import Credential #imports Credential class for testing
+
+class TestCredential(unittest.TestCase):
+    '''
+    Test class that helps define test cases for the credentials class behaviours
+    
+    Args:
+        Testcase class that helps create test cases
+    '''
