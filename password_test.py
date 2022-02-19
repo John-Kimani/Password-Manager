3f0b58220129c95be9657a3f093fbce21b398ac2
John-Kimani <kimwaweru100@gmail.com>
2022-02-19 22:58:51 +0300

    tests case to test in new account object property have been properly instanciated

--- password_test.py
+++ password_test.py
@@ -8,3 +8,23 @@ class TestCredential(unittest.TestCase):
     Args:
         Testcase class that helps create test cases
     '''
+
+    #test to check if credential object is instantiated properly
+    def setup(self):
+        '''
+        Set up method to run before each test case.
+        '''
+        self.new_account = Credential('Pintrest', 'kimperria', 'Aura-Dev98') #sample login details for a new pintrest account 
+    
+    def test__init__(self):
+        '''
+        Test case to test if credential object is instantiated correctly
+        '''
+        self.assertEqual(self.new_account.accountName, 'Pintrest')
+        self.assertEqual(self.new_account.accountUsername,'kimperria')
+        self.assertEqual(self.new_account.accountPassword,'Aura-Dev98')
+
+
+#class condition
+if __name__ == '__main__':
+    unittest.main()
