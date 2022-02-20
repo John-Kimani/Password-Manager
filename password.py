77096bcc83ab05991e9230ab90bf4f9509a6840f
John-Kimani <kimwaweru100@gmail.com>
2022-02-20 03:22:46 +0300

    creates profile list and pass in requirements to creating a new profile

--- password.py
+++ password.py
@@ -2,7 +2,7 @@ class User:
     '''
     This class helps generate instances for creating a new account on this appliation
     '''
-
+    profile_list = [] # empty profile list
     def __init__(self, userName, password):
         '''
         This function helps define properties to create a PASSWORD-MANAGER account.
