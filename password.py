481e901581f1734a6c87e8606acfa40847de2221
John-Kimani <kimwaweru100@gmail.com>
2022-02-20 03:09:32 +0300

    defines init function in user class

--- password.py
+++ password.py
@@ -3,8 +3,20 @@ class User:
     This class helps generate instances for creating a new account on this appliation
     '''
 
+    def __init__(self, userName, password):
+        '''
+        This function helps define properties to create a PASSWORD-MANAGER account.
+        Args: 
+            userName: user's username
+            password: user's password
+        '''
+        self.userName = userName
+        self.password = password
 
 
+
+# end of users instance
+#starts of accounts credentials
 ## generate credentials instances
 class Credential:
     '''
